6aea965e1937890aa2e7cf3db8a4361a0c9bda26
b1conrad <bruce_conrad@byu.edu>
2019-10-10 14:00:59 -0600

    server side option

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+var options = { root: path.join(__dirname,'public') };
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html',options);
+});
+
+app.get('/agent', function(req, res) {
+  res.sendFile('agent.html',options);
+});
+
+app.get('/pico-engine.js', function(req, res) {
+  res.sendFile('pico-engine.js',options);
+});
+
+app.listen(1800, function() {
+  console.log('Example app listening on port 1800!');
+});
